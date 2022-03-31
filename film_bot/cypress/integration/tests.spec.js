5bc165157ff9d57f188fd129084fbc069d4fa745
natha5 <ns539@canterbury.ac.uk>
2022-03-31 13:47:54 +0100

    Added tests for logging in

--- film_bot/cypress/integration/tests.spec.js
+++ film_bot/cypress/integration/tests.spec.js
@@ -4,3 +4,25 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+
+describe('first test', () => {
+	it('visits fb page ', () => {
+		cy.visit('https://www.facebook.com/Testing_bots-108787575008296')
+		cy.get('button').contains('Only Allow Essential Cookies').click()
+		cy.reload()
+		
+		
+	})
+
+	it('logs in with facebook account', () => {
+		//const email
+		//const password
+		
+		
+		cy.get('button').contains('Log In').click
+		//cy.get('input[email=').type(email)
+		
+		//cy.get('input[pass=').type(password)
+	})
+}) 
