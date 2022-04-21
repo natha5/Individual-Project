74257302cdef0218531ee808cb4d3c4e7ecd3032
natha5 <nathan.j.shuttleworth@gmail.com>
2022-04-21 09:54:11 +0000

    Re-implemented random system

--- film_bot/filmBot/index.js
+++ film_bot/filmBot/index.js
@@ -14,127 +14,32 @@ const bot = new BootBot({
   appSecret: config.get('appSecret')
 });
 
-
-let id;
-
-bot.hear('random', (payload,chat) => {
-        fetch(movieRecommendation+id)
-                .then(res => res.json())
-                .then(json => {
-
-                        chat.sendGenericTemplate([{
-                                title: json.results[0].original_title,
-                                subtitle: json.results[0].overview,
-                                image_url:'http://image.tmdb.org/t/p/w500'+json.results[0].poster_path
-                        }]);
-
-                });
-});
-
 bot.on('message', (payload, chat) => {
-  const text = payload.message.text;
-  chat.say(`Hello, what genre of film would you like to watch?`);
+	const text = payload.message.text;
+	console.log(`Welcome, type the genre you would like to watch to begin`);
+        console.log(`Alternatively, type "random" to get a random recommendation`,{typing:true})
 });
 
-
-
- bot.hear('message', (payload, chat) => {
-
-	const askGenre = (convo) => {
-		convo.ask(`What film genre would you like to watch?`, (payload, convo) => {
-			const text = payload.message.text;
-			convo.set('genre', text);
-			convo.say(`Ok, the genre will be ${text}`).then(() => askFavoriteFood(convo));
-
-      
-      //switch statement to match the genre stated to the genre ID
-      switch(res) {
-        case "comedy":
-                id = 35;
-                break;
-        case "action":
-                id = 28;
-                break;
-        case "adventure":
-                id = 12;
-                break;
-        case "animation":
-                id = 16;
-                break;
-        case "crime":
-                id = 80;
-                break;
-        case "documentary":
-                id = 99;
-                break;
-        case "drama":
-                id = 18;
-                break;
-        case "family":
-                id = 10751;
-                break;
-        case "fantasy":
-                id = 14;
-                break;
-        case "history":
-                id = 36;
-                break;
-        case "horror":
-                id = 27;
-                break;
-        case "music":
-                id = 10402;
-                break;
-        case "mystery":
-                id = 9648;
-                break;
-        case "romance":
-                id = 10749;
-                break;
-        case "science fiction":
-                id = 878;
-                break;
-        case "tv movie":
-                id = 10770;
-                break;
-        case "thriller":
-                id = 53;
-                break;
-        case "war":
-                id = 10752;
-                break;
-        case "western":
-                id = 37;
-                break;
-        default:
-chat.say('That is not a genre.');
-        }
-
+bot.hear(['random', 'Random'], (payload,chat) => {
+        const id = Math.floor(Math.random() * 1000);
         if(id!=undefined){
 
+                //make the API request and store the response.
                 fetch(movieRecommendation+id)
                 .then(res => res.json())
-                .then(json => {
-
+                .then(json =>{
+            
+            
+                    //sends the movie title, overview and image in a facebook messenger generic template
                         chat.sendGenericTemplate([{
-                                title: json.results[0].original_title,
-                                subtitle: json.results[0].overview,
-                                image_url:'http://image.tmdb.org/t/p/w500'+json.results[0].poster_path
-                        }]);
+                        title: json.results[0].original_title,
+                        subtitle: json.results[0].overview,
+                        image_url:'http://image.tmdb.org/t/p/w500'+json.results[0].poster_path      
+                  }]);
+            
+                
+})
 
-                });
-        }
 
 
-
-	
-  /*const askLength = (convo) => {
-		convo.ask(`How long do you want the film to be at most (in minutes)?`, (payload, convo) => {
-			const text = payload.message.text;
-			convo.set('length', text);
-			convo.say(`Got it, the film will be no longer than ${text}`).then(() => sendSummary(convo));
-		});
-	};
-
-});*/
 bot.start(config.get('botPort'));
