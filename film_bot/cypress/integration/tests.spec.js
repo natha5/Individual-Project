1ddb4b5b8583a292db44ec7f89c0fbd3b488566e
natha5 <ns539@canterbury.ac.uk>
2022-05-04 10:45:15 +0100

    <mod> Wrote random and genre tests

--- film_bot/cypress/integration/tests.spec.js
+++ film_bot/cypress/integration/tests.spec.js
@@ -7,6 +7,7 @@
 
 
 describe('first test', () => {
+	const prelimTests = {
 	it('visits fb page ', () => {
 		cy.visit('https://www.facebook.com/Testing_bots-108787575008296')
 		cy.get('button').contains('Only Allow Essential Cookies').click()
@@ -24,5 +25,54 @@ describe('first test', () => {
 		//cy.get('input[email=').type(email)
 		
 		//cy.get('input[pass=').type(password)
-	})
+
+	}) }
 }) 
+
+
+describe('random tests' () => {
+	it('tests fully random run', () => {
+		cy.contains('Aa'.click().type('hello').type({enter})
+		cy.contains('Aa'.click().type('random').type({enter})
+	}
+		
+		
+	it('specific genre random film' () => {
+		cy.contains('Aa'.click().type('hello').type({enter})
+		cy.contains('Aa'.click().type('genre comedy').type({enter})
+		cy.contains('Aa'.click().type('random').type({enter})
+	}
+		
+	it('specific genre and length random film' () => {
+		cy.contains('Aa'.click().type('hello').type({enter})
+		cy.contains('Aa'.click().type('genre comedy').type({enter})
+		cy.contains('Aa'.click().type('120').type({enter})
+		cy.contains('Aa'.click().type('random').type({enter})
+	}
+	
+}
+
+describe('gnere validation tests' () => {
+	it('validGenreWithoutPrefix', () => {
+		cy.contains('Aa'.click().type('hello').type({enter})
+		cy.contains('Aa'.click().type('crime').type({enter})
+	}
+	
+	it('invalid genre without prefix', () => {
+		cy.contains('Aa'.click().type('hello').type({enter})
+		cy.contains('Aa'.click().type('banana').type({enter})
+	}
+	
+	it('valid genre with prefix', () => {
+		cy.contains('Aa'.click().type('hello').type({enter})
+		cy.contains('Aa'.click().type('genre crime').type({enter})
+		
+	}
+	it('invalid genre with prefix', () => {
+		cy.contains('Aa'.click().type('hello').type({enter})
+		cy.contains('Aa'.click().type('genre banana').type({enter})
+		
+	}
+	
+describe('length tests' () => {
+	it
