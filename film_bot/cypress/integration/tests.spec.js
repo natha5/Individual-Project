f1b56be54966c3f4ddd02518553d859b3bbedffd
natha5 <72127367+natha5@users.noreply.github.com>
2022-05-06 11:57:42 +0100

    Update tests.spec.js

--- film_bot/cypress/integration/tests.spec.js
+++ film_bot/cypress/integration/tests.spec.js
@@ -1,9 +1,4 @@
 // tests.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
 
 
 describe('first test', () => {
